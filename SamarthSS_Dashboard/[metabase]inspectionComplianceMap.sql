6aae90a34a1f5545e2bf0869b96786acf764fde3
Saif Khan <saif@samagragovernance.in>
2019-01-21 12:08:34 +0530

    Changed district compliance code

--- SamarthSS_Dashboard/[metabase]inspectionComplianceMap.sql
+++ SamarthSS_Dashboard/[metabase]inspectionComplianceMap.sql
@@ -1,23 +1,24 @@
-SELECT District, count(distinct InspectionID), 
+SELECT District, count(distinct InspectionID), @month_diff := max(months),
 CASE 
-WHEN District = "BILASPUR (H.P.)" THEN count(distinct InspectionID)*100/(18*5*months)
-WHEN District = "CHAMBA" THEN count(distinct InspectionID)*100/(18*15*months)
-WHEN District = "HAMIRPUR (H.P.)" THEN count(distinct InspectionID)*100/(18*6*months)
-WHEN District = "KANGRA" THEN count(distinct InspectionID)*100/(18*19*months)
-WHEN District = "KINNAUR" THEN count(distinct InspectionID)*100/(18*3*months)
-WHEN District = "KULLU" THEN count(distinct InspectionID)*100/(18*6*months)
-WHEN District = "MANDI" THEN count(distinct InspectionID)*100/(18*21*months)
-WHEN District = "SHIMLA" THEN count(distinct InspectionID)*100/(18*21*months)
-WHEN District = "SIRMAUR" THEN count(distinct InspectionID)*100/(18*14*months)
-WHEN District = "SOLAN" THEN count(distinct InspectionID)*100/(18*8*months)
-WHEN District = "UNA" THEN count(distinct InspectionID)*100/(18*6*months)
-WHEN District = "LAHUL & SPITI" THEN count(distinct InspectionID)*100/(18*4*months)
+WHEN District = "BILASPUR (H.P.)" THEN count(distinct InspectionID)*100/(18*5*@month_diff)
+WHEN District = "CHAMBA" THEN count(distinct InspectionID)*100/(18*15*@month_diff)
+WHEN District = "HAMIRPUR (H.P.)" THEN count(distinct InspectionID)*100/(18*6*@month_diff)
+WHEN District = "KANGRA" THEN count(distinct InspectionID)*100/(18*19*@month_diff)
+WHEN District = "KINNAUR" THEN count(distinct InspectionID)*100/(18*3*@month_diff)
+WHEN District = "KULLU" THEN count(distinct InspectionID)*100/(18*6*@month_diff)
+WHEN District = "MANDI" THEN count(distinct InspectionID)*100/(18*21*@month_diff)
+WHEN District = "SHIMLA" THEN count(distinct InspectionID)*100/(18*21*@month_diff)
+WHEN District = "SIRMAUR" THEN count(distinct InspectionID)*100/(18*14*@month_diff)
+WHEN District = "SOLAN" THEN count(distinct InspectionID)*100/(18*8*@month_diff)
+WHEN District = "UNA" THEN count(distinct InspectionID)*100/(18*6*@month_diff)
+WHEN District = "LAHUL & SPITI" THEN count(distinct InspectionID)*100/(18*4*@month_diff)
 END as Compliance_percentage
 FROM (
 select *, period_diff(date_format(now(), '%Y%m'), date_format(InspectionDate, '%Y%m')) as months
 from
 shikshaSaathi
 WHERE {{InspectionDate}}
-and {{Designation}} ) as abc
-GROUP BY District, months
+and {{Designation}} 
+) as abc
+GROUP BY District
 ORDER BY District ASC
