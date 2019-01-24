8c61495eee447503abff4a8eb668cce935b98804
Saif Khan <saif@samagragovernance.in>
2019-01-24 11:49:07 +0530

    Changed block code

--- SamarthSS_Dashboard/[metabase]blockgraph.sql
+++ SamarthSS_Dashboard/[metabase]blockgraph.sql
@@ -1,9 +1,9 @@
 Select * from
-(SELECT `shikshaSaathi`.`Designation` AS `Designation`,`shikshaSaathi`.`InspectionDate` AS `InspectionDate`,`shikshaSaathi`.`District` AS `District`, `shikshaSaathi`.`Block` AS `Block`, count(distinct `shikshaSaathi`.`InspectionID`) AS `count`
+(SELECT Block, count(distinct `shikshaSaathi`.`InspectionID`) AS `count`
 FROM `shikshaSaathi`
 where {{District}}
 and {{InspectionDate}}
 and {{Designation}}
-GROUP BY Designation, InspectionDate, District, `shikshaSaathi`.`Block`) as abc
+GROUP BY `shikshaSaathi`.`Block`) as abc
 order by count desc
 LIMIT 20
