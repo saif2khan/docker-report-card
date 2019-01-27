97d138d6993efd4074af0cddd7059b777d80a67b
Saif Khan <saif@samagragovernance.in>
2019-01-27 19:21:12 +0530

    Updated azkaban project code- picks up data from gsheet and creates/replaces a mysql table on another server

--- Azkaban-metabase/testscript.py
+++ Azkaban-metabase/testscript.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from io import BytesIO
 import requests
+import sqlalchemy
 
 r = requests.get('https://docs.google.com/spreadsheets/d/1dadyFbXBI0K6XAp4xkDEBGfmzG6waGk9CJARgFcDocc/export?format=csv')
 data = r.content
@@ -8,3 +9,11 @@ df = pd.read_csv(BytesIO(data))
 
 
 df.to_csv("export.csv", encoding='utf-8', index=False)
+
+database_username = 'azkaban'
+database_password = 'password'
+database_ip       = '139.59.71.154'
+database_name     = 'foo'
+database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
+format(database_username, database_password, database_ip, database_name))
+df.to_sql(con=database_connection, name='apttest', if_exists='replace')
